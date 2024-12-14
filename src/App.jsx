6538776532f4a3ffd26aefa1d0c2c36f81ed8859
Kujonick <przemek.r@opoczta.pl>
2024-12-14 12:32:43 +0100

    clean up the code

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Modes } from "./types";
 import "./App.css";
 import JSZip from "jszip";
 
-// var ip = "34.118.83.75";
-var ip = "127.0.0.1";
+var ip = "34.118.83.75";
+// var ip = "127.0.0.1";
 
 const ColorChangingButton = ({ patientId }) => {
   const [color, setColor] = useState("#191919");
@@ -62,45 +62,6 @@ const ColorChangingButton = ({ patientId }) => {
   );
 };
 
-// function PatientsList({ goToPatient }) {
-//   const [patients, setPatients] = useState([]);
-//   const [listOfPatients, setListOfPatients] = useState([]);
-
-//   setListOfPatients(() =>
-//     patients.map((patient) => {
-//       return (
-//         <li key={patient.id}>
-//           <button onClick={() => goToPatient({ patient })}>
-//             {patient.name}
-//           </button>
-//           <h2>danger: {Math.round(patient.danger + Number.EPSILON) / 100}</h2>
-//           <ColorChangingButton patientId={patient.id}></ColorChangingButton>
-//         </li>
-//       );
-//     })
-//   );
-
-//   async function fetchPatients() {
-//     const response = await fetch(`http://${ip}:8000/patients`);
-//     const data = await response.json();
-//     setPatients(data);
-//   }
-
-//   useEffect(() => {
-//     fetchPatients();
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="listMenu">
-//         <button onClick={fetchPatients}>Reload patients</button>
-//       </div>
-//       <div>
-//         <ol>{listOfPatients}</ol>
-//       </div>
-//     </>
-//   );
-// }
 function PatientsList({ goToPatient }) {
   const [patients, setPatients] = useState([]);
 
@@ -112,7 +73,7 @@ function PatientsList({ goToPatient }) {
 
   useEffect(() => {
     fetchPatients();
-  }, []); // Wywołaj fetchPatients tylko raz, po zamontowaniu komponentu
+  }, []);
 
   return (
     <>
